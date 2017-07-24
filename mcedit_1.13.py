e7925f38888ef524428b12f9e38ae1cd98b0632a
chokoboy3 <alexanderthomsen15@hotmail.com>
2017-07-24 23:51:39 +0200

    Update mcedit_1.13.py

--- mcedit_1.13.py
+++ mcedit_1.13.py
@@ -15,9 +15,11 @@ displayName = "1.12 to 1.13"
 inputs = (
     ("Updates command and structure blocks to 1.13", "label"),
     ("Datapack name (Alphanumeric) ", ("string","value=")),
+    ("Check the console for error messages", "label"),
 )
 
 def perform(level, box, options):
+    global datapack
     datapack = options["Datapack name (Alphanumeric) "]
     
     for (chunk, slices, point) in level.getChunkSlices(box):
@@ -29,7 +31,7 @@ def perform(level, box, options):
             if (x,y,z) in box:
                 if e["id"].value in ["minecraft:command_block","minecraft:chain_command_block","minecraft:repeating_command_block"]:
 
-                    e["Command"] = TAG_String(convert(e["Command"].value))
+                    e["Command"] = TAG_String(convert_command(e["Command"].value, "{} {} {}".format(x, y, z)))
                     chunk.dirty = True
                     
                 elif e["id"].value == "minecraft:structure_block":
@@ -37,12 +39,19 @@ def perform(level, box, options):
                     chunk.dirty = True
 
 
+
+
 #Set values
 effect_id = ('speed', 'slowness', 'haste', 'mining_fatigue', 'strength', 'instant_health', 'instant_damage', 'jump_boost', 'nausea', 'regeneration', 'resistance', 'fire_resistance', 'water_breathing', 'invisibility', 'blindness', 'night_vision', 'hunger', 'weakness', 'poison', 'wither', 'health_boost', 'absorption', 'saturation', 'glowing', 'levitation', 'luck', 'unluck')
+
 color = ("white","orange","magenta","light_blue","yellow","lime","pink","gray","silver","cyan","purple","blue","brown","green","red","black")
 facing = ("north","north","north","south","west","east","north","north","north","south","west","east","north","north","north","south")
+leaves = ("check_decay=false,decayable=true","check_decay=false,decayable=false","check_decay=true,decayable=true","check_decay=true,decayable=false")
+door = ("half=lower,open=false,facing=east","half=lower,open=false,facing=south","half=lower,open=false,facing=west","half=lower,open=false,facing=north","half=lower,open=true,facing=east","half=lower,open=true,facing=south","half=lower,open=true,facing=west","half=lower,open=true,facing=north","half=upper,hinge=left,powered=false","half=upper,hinge=right,powered=false","half=upper,hinge=left,powered=true","half=upper,hinge=right,powered=true","half=upper,hinge=left,powered=false","half=upper,hinge=right,powered=false","half=upper,hinge=left,powered=true","half=upper,hinge=right,powered=true") #Doors are confusing
+button = ("powered=false,facing=down","powered=false,facing=east","powered=false,facing=west","powered=false,facing=south","powered=false,facing=north","powered=false,facing=up","powered=false,facing=up","powered=false,facing=up","powered=true,facing=down","powered=true,facing=east","powered=true,facing=west","powered=true,facing=south","powered=true,facing=north","powered=true,facing=up","powered=true,facing=up","powered=true,facing=up")
 
-def change_block(block, data, nbt):
+
+def change_block(block, data, nbt, filename):
     if data.isdigit():
         data = int(data)
     elif len(data) == 0:
@@ -55,8 +64,26 @@ def change_block(block, data, nbt):
     elif isinstance( data, int ):
         if block in ("wool","stained_glass","stained_hardened_clay","concrete","concrete_powder","stained_glass_pane","carpet"):
             block = "{0}_{1}".format(color[data], block)
-        elif block in ("chest","furnace","ladder","ender_chest"):
+        elif block in ("chest","furnace","ladder","ender_chest","trapped_chest","wall_sign","wall_banner"):
             block += "[facing={}]".format( facing[data] )
+        elif block in ("water", "lava"):
+            block += "[level={}]".format(data % 8) #This is not entirely correct but unless someone uses "water 15" to place regular water, it's going to work
+        elif block == "leaves":
+            block = "{0}_leaves[{1}]".format( ("oak","spruce","birch","jungle")[data % 4], leaves[int(data / 4)] )
+        elif block == "leaves2":
+            block = "{0}_leaves[{1}]".format( ("acacia","dark_oak")[data % 2], leaves[int(data / 4)] )
+        elif block in ("redstone_wire","heavy_weighted_pressure_plate","light_weighted_pressure_plate"):
+            block += "[power={}]".format(data)
+        elif block in ("standing_sign","standing_banner"):
+            block += "[rotation={}]".format(data)
+        elif block in ("wooden_door","iron_door","spruce_door","birch_door","jungle_door","acacia_door","dark_oak_door"):
+            block += "[{}]".format(door[data])
+        elif block in ("stone_button","wooden_button"):
+            block += "[{}]".format(button[data])
+
+        elif data > 0:
+            print("[Error] Some data was lost: {} {} in {}".format(block, data, filename))
+
     else:
         arg = data.split("=")
         if arg[0] == "color":
@@ -69,10 +96,8 @@ def change_block(block, data, nbt):
 
     return block
 
-def convert_command(command):
-    global effect_id
-    global color
-    global facing
+def convert_command(command, filename):
+
 
     if not command.startswith("#"):
         try:
@@ -86,7 +111,7 @@ def convert_command(command):
             #Gamemode
             command = re.sub(r'gamemode 0',r'gamemode survival',command)
             command = re.sub(r'gamemode 1',r'gamemode creative',command)
-            command = re.sub(r'gamemode 2',r'gamemode adeventure',command)
+            command = re.sub(r'gamemode 2',r'gamemode adventure',command)
             command = re.sub(r'gamemode 3',r'gamemode spectator',command)
 
             #Difficulty
@@ -95,8 +120,6 @@ def convert_command(command):
             command = re.sub(r'difficulty (2|n)',r'difficulty normal',command)
             command = re.sub(r'difficulty (3|h)',r'difficulty hard',command)
 
-
-            #Probably what I need to do is to not change x y z if dx dy dz are included and then add 1 to dx dy dz to simulate how they work in 1.12
 
             #dx dy dz
             tmp = re.findall(r'@([a-z])\[([A-Za-z0-9=\.,_\-\!]*)\]', command)
@@ -111,15 +134,15 @@ def convert_command(command):
                         selector += ["dy=0"]
                     if not "dz" in match[1]:
                         selector += ["dz=0"]
-                    
+
                 if "dx" in match[1]:
-                    
+
                     for i in range(len(selector)):
                         arg = selector[i].split("=")
                         if arg[0] in ["dx","dy","dz"]:
-                            
+
                             selector[i] = "{}={}".format(arg[0],int(arg[1]) + 1)
-    
+
                 else:
                     #Int coordinates to floats if needed
                     for i in range(len(selector)):
@@ -128,7 +151,7 @@ def convert_command(command):
                             selector[i] = "{}={}".format(arg[0],float(arg[1]) + 0.5)
 
 
-                
+
                 command = re.sub(r'pl@ceh0ld3r', ",".join(selector), command, count=1)
 
 
@@ -148,7 +171,7 @@ def convert_command(command):
                 command = re.sub(r'effect @([a-z])([A-Za-z0-9=\.,_\-\!\[\]]*) ([0-9]+) 0(.*)', r'effect clear @\1\2 \3', command)
                 #Effect give
                 command = re.sub(r'effect @([a-z])([A-Za-z0-9=\.,_\-\!\[\]]*) ([0-9]+) ([1-9][0-9]*)', r'effect give @\1\2 \3 \4', command)
-                command = re.sub(r'effect @([a-z])([A-Za-z0-9=\.,_\-\!\[\]]*) ([0-9]+)$', r'effect give @\1\2 \3', command)
+                command = re.sub(r'effect @([a-z])([A-Za-z0-9=\.,_\-\!\[\]]*) ([0-9]+)$', r'effect give @\1\2 \3 30 0', command)
 
                 #Effect ID's
                 tmp = re.findall(r'effect (give|clear) @([a-z])([A-Za-z0-9=\.,_\-\!\[\]]*) ([0-9]+)', command)
@@ -162,13 +185,13 @@ def convert_command(command):
             command = re.sub(r'function ([A-Za-z_]+):([A-Za-z_/]+)', r'function {}:functions/\1/\2'.format(datapack), command)
             command = re.sub(r'advancement (.*) ([A-Za-z_]+):([A-Za-z_/]+)', r'advancement \1 {}:advancements/\2/\3'.format(datapack), command)
             command = re.sub(r'LootTable:"([A-Za-z_]+):([A-Za-z_/]+)"', r'LootTable:"{}:loot_tables/\1/\2"'.format(datapack), command)
-            
+
             command = re.sub(r'structure_block(.+)name:(?:")?([A-Za-z]+):([A-Za-z_/]+)(?:")?', r'structure_block\1name:"\2/\3"', command)
             command = re.sub(r'structure_block(.+)name:(?:")?([A-Za-z_/]+)(?:")?', r'structure_block\1name:"{}:structures/\2"'.format(datapack), command)
 
 
 
-            #Scoreboard and testfor NBT
+            #Scoreboard NBT
             if "scoreboard" in command:
 
 
@@ -190,8 +213,9 @@ def convert_command(command):
                     else:
                         command = re.sub(r'pl@ceh0ld3r', "[nbt={0}]".format(tmp[0][5]), command)
 
+            #testfor NBT
             if "testfor" in command:
-                #testfor
+
                 tmp = re.findall(r'testfor @([a-z])([A-Za-z0-9=\.,_\-\!\[\]]*) ({.*})', command)
                 command = re.sub(r'testfor @([a-z])([A-Za-z0-9=\.,_\-\!\[\]]*) ({.*})', r'testfor @\1pl@ceh0ld3r', command)
                 if tmp:
@@ -200,13 +224,13 @@ def convert_command(command):
                     else:
                         command = re.sub(r'pl@ceh0ld3r', "[nbt={0}]".format(tmp[0][2]), command)
 
-            #Execute remove slashes
+            #Remove forward slashes in execute
             command = re.sub(r'execute @([a-z])([A-Za-z0-9=\.,_\-\!\[\]]*) ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) /', r'execute @\1\2 \3 ', command)
             command = re.sub(r'detect ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) ([a-zA-Z\:]+) ([0-9]+) /', r'detect \1 \2 \3 ', command)
 
 
 
-            #Execute to as and at. This also tries to optimize it a little
+            #Execute to "as" and "at". This also tries to optimize it a little
             tmp = re.findall(r'execute @([a-z])([A-Za-z0-9=\.,_\-\!\[\]]*) ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) (.*)', command)
             for match in tmp:
                 execute = match[3]
@@ -218,32 +242,32 @@ def convert_command(command):
                 else:
                     offset = True
                     useat = True
-                
 
-                
+
+
                 if re.findall(r'(~|dx=|dy=|dz=|c=|r=|rm=)', execute):
                     useat = True
                 elif execute.startswith("function"):
                     useat = True
 
 
-                
+
                 if re.findall(r'@s', execute):
                     useas = True
-                    
+
                 if any(execute.startswith(i) for i in ("setblock","fill","clone","blockdata","summon")):
                     useas = False
 
-                
+
 
                 if useas == False and useat == False and len(match[1]) > 2:
                     useat = True
 
 
-                
+
                 if useas == False and useat == False:
                     command = re.sub(r'execute @([a-z])([A-Za-z0-9=\.,_\-\!\[\]]*) ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) ',r'', command) #Remove execute if it was only used for detecting a block for example
-                    
+
                 elif useas == True and useat == False:
                     command = re.sub(r'execute @([a-z])([A-Za-z0-9=\.,_\-\!\[\]]*) ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) ', r'as @\1\2 ', command) #No at if relative coordinates weren't used
 
@@ -253,7 +277,7 @@ def convert_command(command):
                         command = re.sub(r'execute @([a-z])([A-Za-z0-9=\.,_\-\!\[\]]*) ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) ', r'at @\1\2 ', command) #No at if relative coordinates weren't used
                     else:
                         command = re.sub(r'execute @([a-z])([A-Za-z0-9=\.,_\-\!\[\]]*) ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) ', r'at @\1\2 offset \3 ', command) #Same with offset
-                    
+
                 elif useas == True and useat == True:
 
                     if offset == False:
@@ -261,7 +285,7 @@ def convert_command(command):
                     else:
                         command = re.sub(r'execute @([a-z])([A-Za-z0-9=\.,_\-\!\[\]]*) ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) ', r'as @\1\2 at @s offset \3 ', command) #Same with offset
 
-    
+
 
 
 
@@ -271,39 +295,39 @@ def convert_command(command):
                 tmp = re.findall(r'setblock ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) (?:minecraft\:)?([A-Za-z_]+)(?: )?([\S]+)?(?: )?(destroy|keep|replace)?(?: )?(.+)?', command)
                 command = re.sub(r'setblock ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) (?:minecraft\:)?([A-Za-z_]+)(?: )?([\S]+)?(?: )?(destroy|keep|replace)?(?: )?(.+)?', r'setblock \1 minecraft:pl@ceh0ld3r \4', command)
                 if tmp:
-                    command = re.sub(r'pl@ceh0ld3r', change_block(tmp[0][1], tmp[0][2], tmp[0][4]), command)
+                    command = re.sub(r'pl@ceh0ld3r', change_block(tmp[0][1], tmp[0][2], tmp[0][4], filename), command)
 
             if "detect" in command:
                 tmp = re.findall(r'detect ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) (?:minecraft\:)?([A-Za-z_]+) ([\S]+)', command)
                 command = re.sub(r'detect ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) (?:minecraft\:)?([A-Za-z_]+) ([\S]+)', r'detect \1 minecraft:pl@ceh0ld3r', command)
                 if tmp:
-                    command = re.sub(r'pl@ceh0ld3r', change_block(tmp[0][1], tmp[0][2], ""), command)
+                    command = re.sub(r'pl@ceh0ld3r', change_block(tmp[0][1], tmp[0][2], "", filename), command)
 
             if "testforblock " in command:
                 tmp = re.findall(r'testforblock ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) (?:minecraft\:)?([A-Za-z_]+)(?: )?([\S]+)?(?: )?({[.*]})?', command)
                 command = re.sub(r'testforblock ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) (?:minecraft\:)?([A-Za-z_]+)(?: )?([\S]+)?(?: )?({[.*]})?', r'testforblock \1 minecraft:pl@ceh0ld3r', command)
                 if tmp:
-                    command = re.sub(r'pl@ceh0ld3r', change_block(tmp[0][1], tmp[0][2], tmp[0][3]), command)
+                    command = re.sub(r'pl@ceh0ld3r', change_block(tmp[0][1], tmp[0][2], tmp[0][3], filename), command)
 
             if "fill" in command:
                 #Fill replace
                 tmp = re.findall(r'fill ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) (?:minecraft\:)?([A-Za-z_]+) ([\S]+) replace (?:minecraft\:)?([A-Za-z_]+)(?: )?([\S]+)?', command)
                 command = re.sub(r'fill ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) (?:minecraft\:)?([A-Za-z_]+) ([\S]+) replace (?:minecraft\:)?([A-Za-z_]+)(?: )?([\S]+)?', r'fill \1 minecraft:pl@ceh0ld3r replace minecraft:pl@ceh0ld2r', command)
                 if tmp:
-                    command = re.sub(r'pl@ceh0ld3r', change_block(tmp[0][1], tmp[0][2], ""), command)
-                    command = re.sub(r'pl@ceh0ld2r', change_block(tmp[0][3], tmp[0][4], ""), command)
+                    command = re.sub(r'pl@ceh0ld3r', change_block(tmp[0][1], tmp[0][2], "", filename), command)
+                    command = re.sub(r'pl@ceh0ld2r', change_block(tmp[0][3], tmp[0][4], "", filename), command)
                 else:
                     #Fill normal
                     tmp = re.findall(r'fill ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) (?:minecraft\:)?([A-Za-z_]+) ([\S]+)(?: )?(destroy|hollow|keep|outline|replace)?(?: )?({.+})?', command)
                     command = re.sub(r'fill ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) (?:minecraft\:)?([A-Za-z_]+) ([\S]+)(?: )?(destroy|hollow|keep|outline|replace)?(?: )?({.+})?', r'fill \1 minecraft:pl@ceh0ld3r \4', command)
                     if tmp:
-                        command = re.sub(r'pl@ceh0ld3r', change_block(tmp[0][1], tmp[0][2], tmp[0][4]), command)
+                        command = re.sub(r'pl@ceh0ld3r', change_block(tmp[0][1], tmp[0][2], tmp[0][4], filename), command)
 
             #Filtered clone
             tmp = re.findall(r'clone ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) (filtered|replace|masked) (force|move|normal) (?:minecraft\:)?([A-Za-z_]+)(?: )?([\S]+)?', command)
             command = re.sub(r'clone ([~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+ [~\-0-9\.]+) (filtered|replace|masked) (force|move|normal) (?:minecraft\:)?([A-Za-z_]+)(?: )?([\S]+)?', r'clone \1 filtered \3 minecraft:pl@ceh0ld3r', command)
             if tmp:
-                command = re.sub(r'pl@ceh0ld3r', change_block(tmp[0][3], tmp[0][4], ""), command)
+                command = re.sub(r'pl@ceh0ld3r', change_block(tmp[0][3], tmp[0][4], "", filename), command)
 
 
 
